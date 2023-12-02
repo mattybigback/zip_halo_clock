297b00434be2afd4092e702ae6a2624747851792
Matt Harrison <mattybigback@gmail.com>
2023-12-02 22:39:03 +0000

    minified constant names

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ from microbit import pin8, pin14, sleep, button_a, button_b, display
 from neopixel import NeoPixel
 import MCP7940N as rtc
 
-TICK_SND = True
-BRT = 0
-
+SND = True  # Sound on or off
+BRT = 4     # Brightmness (0-4)
 LEDS = 60
 
 
@@ -17,9 +16,9 @@ def setup():
 
 
 def set_markers():
-    MARKS = (0,5,10,15,20,25,30,35,40,45,50,55)
+    MRKS = (0,5,10,15,20,25,30,35,40,45,50,55)
     clear_ring()
-    for i in MARKS:
+    for i in MRKS:
         ring[i] = set_bright_offset([15, 0, 0])
     
 def tick(time):
@@ -29,9 +28,9 @@ def tick(time):
     set_min_sec_led(time[1],"m")
     set_min_sec_led(time[2])
     ring.show()
-    if TICK_SND:
+    if SND:
         pin14.write_analog(127)
-    if TICK_SND is True:
+    if SND is True:
         sleep(20)
         pin14.write_analog(0)
 
@@ -51,9 +50,9 @@ def set_min_sec_led(unit, type="s"):
 def set_hour_led(hour, min):
     hour_led = calc_hour_led(hour, min)
     if hour > 12:
-        hour_col = [0, 0, 30]
+        hour_col = [0, 0, 15]
     else:
-        hour_col = [0,15,15]
+        hour_col = [0, 15, 15]
     ring[hour_led] = set_bright_offset(hour_col)
 
 def clear_ring():
@@ -65,9 +64,9 @@ def check_time():
     return (rtc.cur_hr, rtc.cur_min, rtc.cur_sec)
 
 def toggle_tick(state):
-    global TICK_SND
+    global SND
     display.scroll("N" if state else "Y")
-    TICK_SND = not state
+    SND = not state
 
 def set_bright_offset(colors_in):
     colors_out = [rgb << BRT for rgb in colors_in]
@@ -90,7 +89,7 @@ def menu():
                 display.scroll("T")
                 scrl = True
             if button_b.is_pressed():
-                toggle_tick(TICK_SND)
+                toggle_tick(SND)
 
         if b_cnt == 2:
             if scrl is False:
@@ -147,17 +146,17 @@ def menu():
 
 def main():
     display.clear()
-    ticks = 0
+    tks = 0
     time_now = check_time()
     while True:
         if time_now != check_time():
             time_now = check_time()
             tick(time_now)
             if button_a.is_pressed() and button_b.is_pressed():
-                ticks += 1
-            if ticks >= 2:
+                tks += 1
+            if tks >= 2:
                 menu()
-                ticks = 0
+                tks = 0
 
 setup()
 main()
