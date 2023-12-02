d814fb4fa1a278ab73682abde059cdf43aff13f2
Matt Harrison <mattybigback@gmail.com>
2023-12-02 22:50:26 +0000

    Refactoring

--- main.py
+++ main.py
@@ -1,42 +1,42 @@
-from microbit import pin8, pin14, sleep, button_a, button_b, display
+from microbit import (
+    pin8 as neo_pin,
+    pin14 as buzz,
+    sleep,
+    button_a as btn_a,
+    button_b as btn_b,
+    display as disp)
 from neopixel import NeoPixel
 import MCP7940N as rtc
 
 SND = True  # Sound on or off
-BRT = 4     # Brightmness (0-4)
-LEDS = 60
+BRT = 2     # Brightmness (0-4)
+LEDS = 60   # Number of neopixels to control
 
-
-ring = NeoPixel(pin8, LEDS)
+ring = NeoPixel(neo_pin, LEDS)
 rtc = rtc.MCP7940N()
-
-def setup():
-    rtc.setTime(0,0,0)
-    pin14.set_analog_period_microseconds(10000)
-
-
-def set_markers():
-    MRKS = (0,5,10,15,20,25,30,35,40,45,50,55)
-    clear_ring()
-    for i in MRKS:
-        ring[i] = set_bright_offset([15, 0, 0])
+buzz.set_analog_period_microseconds(10000)
     
 def tick(time):
-
     set_markers()
     set_hour_led(time[0], time[1])
     set_min_sec_led(time[1],"m")
     set_min_sec_led(time[2])
     ring.show()
     if SND:
-        pin14.write_analog(127)
+        buzz.write_analog(127)
     if SND is True:
         sleep(20)
-        pin14.write_analog(0)
+        buzz.write_analog(0)
 
 def calc_hour_led(hr, mn):
     base_hour = (hr % 12) * 5
     return (base_hour + int(mn / 12))
+
+def set_markers():
+    MRKS = (0,5,10,15,20,25,30,35,40,45,50,55)
+    clear_ring()
+    for i in MRKS:
+        ring[i] = set_bright_offset([15, 0, 0])
 
 def set_min_sec_led(unit, type="s"):
     if type == "m":
@@ -45,7 +45,6 @@ def set_min_sec_led(unit, type="s"):
         color = [15,15,15]
     color = set_bright_offset(color)
     ring[unit] = color
-
 
 def set_hour_led(hour, min):
     hour_led = calc_hour_led(hour, min)
@@ -65,13 +64,12 @@ def check_time():
 
 def toggle_tick(state):
     global SND
-    display.scroll("N" if state else "Y")
+    disp.scroll("N" if state else "Y")
     SND = not state
 
 def set_bright_offset(colors_in):
     colors_out = [rgb << BRT for rgb in colors_in]
     return colors_out
-
 
 def menu():
     hr = rtc.cur_hr
@@ -86,19 +84,19 @@ def menu():
     while True:
         if b_cnt == 1:
             if scrl is False:
-                display.scroll("T")
+                disp.scroll("T")
                 scrl = True
-            if button_b.is_pressed():
+            if btn_b.is_pressed():
                 toggle_tick(SND)
 
         if b_cnt == 2:
             if scrl is False:
-                display.scroll("H")
+                disp.scroll("H")
                 scrl = True
                 set_markers()
                 set_hour_led(hr, 0)
                 ring.show()
-            if button_b.is_pressed():
+            if btn_b.is_pressed():
                 hr = (hr + 1) % 24
                 set_markers()
                 set_hour_led(hr, 0)
@@ -107,12 +105,12 @@ def menu():
 
         if b_cnt == 3:
             if scrl is False:
-                display.scroll("M")
+                disp.scroll("M")
                 scrl = True
                 set_markers()
                 set_min_sec_led(mins, "m")
                 ring.show()
-            if button_b.is_pressed():
+            if btn_b.is_pressed():
                 mins = (mins + 1) % 60
                 set_markers()
                 set_min_sec_led(mins, "m")
@@ -122,19 +120,19 @@ def menu():
     
         if b_cnt == 4:
             if scrl is False:
-                display.scroll("S")
+                disp.scroll("S")
                 scrl = True
                 set_markers()
                 set_min_sec_led(sec)
                 ring.show()
-            if button_b.is_pressed():
+            if btn_b.is_pressed():
                 sec = (sec + 1) % 60
                 set_markers()
                 set_min_sec_led(sec)
                 ring.show()
                 sleep(100)
 
-        if button_a.is_pressed() and b_cnt < 5:
+        if btn_a.is_pressed() and b_cnt < 5:
             b_cnt += 1
             scrl  = False
 
@@ -145,18 +143,17 @@ def menu():
 
 
 def main():
-    display.clear()
+    disp.clear()
     tks = 0
     time_now = check_time()
     while True:
         if time_now != check_time():
             time_now = check_time()
             tick(time_now)
-            if button_a.is_pressed() and button_b.is_pressed():
+            if btn_a.is_pressed() and btn_b.is_pressed():
                 tks += 1
             if tks >= 2:
                 menu()
                 tks = 0
 
-setup()
 main()
